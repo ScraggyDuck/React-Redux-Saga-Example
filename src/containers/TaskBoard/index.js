47f5d0dbf7169550568e5db9a856f4d33d7690af
JokerV <Quocvietcoi2000@gmail.com>
2019-09-28 12:27:57 +0700

    Building interfaces p2

--- src/containers/TaskBoard/index.js
+++ src/containers/TaskBoard/index.js
@@ -4,19 +4,84 @@ import styles from './styles';
 import { STATUSES } from '../../constants';
 
 import { withStyles } from '@material-ui/core';
-import { Button, Grid } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Box
+} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+
+const listTask = [
+  {
+    id: 0,
+    title: 'Play game',
+    description: 'With my friends',
+    status: 1
+  },
+  {
+    id: 1,
+    title: 'Play game1',
+    description: 'With my friends1',
+    status: 0
+  },
+  {
+    id: 2,
+    title: 'Play game2',
+    description: 'With my friends2',
+    status: 1
+  },
+  {
+    id: 3,
+    title: 'Play game3',
+    description: 'With my friends3',
+    status: 2
+  }
+];
 
 class TaskBoard extends Component {
   renderBoard = () => {
+    const { classes } = this.props;
     let xhtml = null;
     xhtml = (
       <Grid container spacing={2}>
-        {STATUSES.map(status => (
-          <Grid item md={4} xs={12}>
-            {status.label}
-          </Grid>
-        ))}
+        {STATUSES.map(status => {
+          const taskFiltered = listTask.filter(
+            task => task.status === status.value
+          );
+          return (
+            <Grid item md={4} xs={12}>
+              <Box mt={2} mb={2}>
+                <div className={classes.status}>{status.label}</div>
+              </Box>
+              <div className={classes.wrapperListTask}>
+                {taskFiltered.map(task => {
+                  const { id, title, description } = task;
+                  return (
+                    <Card key={id}>
+                      <CardContent>
+                        <Grid container justify='space-between'>
+                          <Grid item md={8}>
+                            <Typography component='h2'>{title}</Typography>
+                          </Grid>
+                          <Grid item md={4}>
+                            {status.label}
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                      <CardActions>
+                        <Button size='small'>Button</Button>
+                      </CardActions>
+                    </Card>
+                  );
+                })}
+              </div>
+            </Grid>
+          );
+        })}
       </Grid>
     );
     return xhtml;
